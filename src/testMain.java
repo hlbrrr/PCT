737f123007047636bd672675531d6064584cca68
Anton <hlbrrr@gmail.com>
2011-12-07 18:29:38 +0600

    New changelist

--- src/testMain.java
+++ src/testMain.java
@@ -1,5 +1,7 @@
 import com.compassplus.configurationModel.Configuration;
 import com.compassplus.gui.MainForm;
+import com.compassplus.gui.ProposalForm;
+import com.compassplus.proposalModel.Proposal;
 import com.compassplus.utils.CommonUtils;
 import com.compassplus.utils.DesEncrypter;
 import com.compassplus.utils.Logger;
@@ -7,6 +9,10 @@ import org.apache.commons.io.FileUtils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 
 /**
@@ -21,39 +27,33 @@ public class testMain {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI();
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception unused) {
+                }
+                proceed();
             }
         });
     }
 
-    private static void createAndShowGUI() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception unused) {
-        }
+    private static void createAndShowGUI(String pwd, JFrame oframe) {
         Configuration config = Configuration.getInstance();
         try {
             try {
-                String pwd = (String) JOptionPane.showInputDialog(null, "Please enter your password", "PCT", JOptionPane.QUESTION_MESSAGE);
-                if (pwd != null) {
-                    DesEncrypter ds = new DesEncrypter(CommonUtils.getInstance().md5(pwd, defaultEnc), defaultEnc);
-                    config.init(CommonUtils.getInstance().getDocumentFromString(ds.decrypt(FileUtils.readFileToString(new File("config.exml"), defaultEnc))));
-                    ds = null;
-                    pwd = null;
-
-                } else {
-                    throw new Exception("NoPassword");
-                }
+                DesEncrypter ds = new DesEncrypter(CommonUtils.getInstance().md5(pwd, defaultEnc), defaultEnc);
+                config.init(CommonUtils.getInstance().getDocumentFromString(ds.decrypt(FileUtils.readFileToString(new File("config.exml"), defaultEnc))));
+                ds = null;
+                pwd = null;
             } catch (Exception e) {
                 Logger.getInstance().error(e);
-                if (e.getMessage() == null || !"NoPassword".equals(e.getMessage())) {
-                    JOptionPane.showMessageDialog(null, "Broken or expired configuration", "Error", JOptionPane.ERROR_MESSAGE);
-                }
+                JOptionPane.showMessageDialog(null, "Broken or expired configuration", "Error", JOptionPane.ERROR_MESSAGE);
+                oframe.dispose();
                 throw e;
             }
 
             MainForm main = new MainForm(config);
-            final JFrame frame = new JFrame("PCT [" + config.getUserName() + "]");
+            final JFrame frame = oframe;
+            frame.setTitle("PCT [" + config.getUserName() + "]");
             main.setFrame(frame);
             main.setExitAction(new AbstractAction() {
                 public void actionPerformed(ActionEvent e) {
@@ -62,12 +62,60 @@ public class testMain {
             });
             frame.setJMenuBar(main.getMenu());
             frame.setContentPane(main.getRoot());
-            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.pack();
             frame.setSize(700, 500);
             frame.setLocationByPlatform(true);
             frame.setVisible(true);
         } catch (Exception e) {
+
         }
+    }
+
+    private static void proceed() {
+        final JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        final JPasswordField pwdField = new JPasswordField();
+        final JOptionPane optionPane = new JOptionPane(
+                new JComponent[]{new JLabel("Password"), pwdField},
+                JOptionPane.QUESTION_MESSAGE,
+                JOptionPane.OK_CANCEL_OPTION);
+        final JDialog dialog = new JDialog(frame, "PCT", true);
+        dialog.setResizable(false);
+        dialog.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                dialog.dispose();
+                frame.dispose();
+            }
+        });
+        dialog.setContentPane(optionPane);
+
+        dialog.setSize(200, 200);
+        dialog.setLocationRelativeTo(null);
+
+        optionPane.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent e) {
+                if (optionPane.getValue() != null) {
+                    String prop = e.getPropertyName();
+                    if (dialog.isVisible()
+                            && (e.getSource() == optionPane)
+                            && (prop.equals(JOptionPane.VALUE_PROPERTY))) {
+                        if (optionPane.getValue() instanceof Integer) {
+                            int value = (Integer) optionPane.getValue();
+                            if (value == JOptionPane.OK_OPTION) {
+                                String pwd = new String(pwdField.getPassword());
+                                dialog.dispose();
+                                createAndShowGUI(pwd, frame);
+                            } else if (value == JOptionPane.CANCEL_OPTION) {
+                                dialog.dispose();
+                                frame.dispose();
+                            }
+                        }
+                        optionPane.setValue(null);
+                    }
+                }
+            }
+        });
+        dialog.pack();
+        dialog.setVisible(true);
     }
 }
